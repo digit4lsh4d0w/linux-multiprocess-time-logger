ce7b12581615110281ed3898fbda2010761b328c
Redrov Ivan <digit4lsh4d0w@ya.ru>
2026-05-03 22:25:02 +1000

    Инициализация варианта на Zig

---
+++ zig-lang/build.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "time-logger",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+    });
+    exe.root_module.linkSystemLibrary("rt", .{});
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the logger");
+    run_step.dependOn(&run_cmd.step);
+}
